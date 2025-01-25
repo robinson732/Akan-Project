75b35a855f742b332a53679658f94131697c6599
Robinson Kimani <kimanirobinson336@gmail.com>
2025-01-25 16:59:23 +0300

    Added Akan names

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,12 @@
 function GenerateAkanName() {
   //Get selected gender
-  const gender = document.querySelector("input[name=gender]");
-}
+  const gender=document.querySelector('input[name="gender"]:checked')?.value;
+  //Check if gender is selected
+  if (!gender) {
+    alert("Select gender!");
+    return; 
+  }
+  // Akan names based on gender and days of the week
+  const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  ss
