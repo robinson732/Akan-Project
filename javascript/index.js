4cf6752c0b26793003f2a09e1c522e74996241ba
Robinson Kimani <kimanirobinson336@gmail.com>
2025-01-25 17:15:37 +0300

    Modifying the day selection

--- javascript/index.js
+++ javascript/index.js
@@ -9,4 +9,8 @@ function GenerateAkanName() {
   // Akan names based on gender and days of the week
   const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  ss
+  // Prompt for days of the week 
+  const day = parseInt(prompt("Enter the day of the week):"));
+  if (isNan(day) || day<0 ||day<6) {
+    alert("Please add a valid day")
+  }
