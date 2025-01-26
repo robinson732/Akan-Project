65e404420fc74f3a69707983aeb6141c540b0a5d
Robinson Kimani <kimanirobinson336@gmail.com>
2025-01-26 22:51:23 +0300

    Prompt for days

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,38 @@
 function GenerateAkanName() {
   //Get selected gender
-  const gender=document.querySelector('input[name="gender"]:checked')?.value;
+  const gender = document.querySelector('input[name="gender"]:checked')?.value;
   //Check if gender is selected
   if (!gender) {
     alert("Select gender!");
-    return; 
+    return;
   }
   // Akan names based on gender and days of the week
-  const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-  const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-  // Prompt for days of the week 
+  const maleNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+  const femaleNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama",
+  ];
+  // Prompt for days of the week
   const day = parseInt(prompt("Enter the day of the week):"));
-  if (isNan(day) || day<0 ||day<6) {
-    alert("Please add a valid day")
+  if (isNan(day) || day < 0 || day < 6) {
+    alert("Please add a valid day");
+    return;
   }
+  //Generate the Akan name
+  const akanName = gender === "male" ? maleNames[day] : femaleNames[day];
+  //Display the Akan name
+  alert("Congratulations your Akan name is: ${akanName}");
+}
